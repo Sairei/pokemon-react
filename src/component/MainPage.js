126572261d7f03ae894eab50de5705cd1fb2367c
Christopher CAUET <christophercauet@hotmail.fr>
2021-12-12 23:06:45 +0100

    :recycle: Clean project

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -2,18 +2,11 @@ import React from 'react';
 
 import { Outlet } from 'react-router';
 
-import Header from './items/Header';
-import Footer from './items/Footer';
-
 class MainPage extends React.Component {
   render() { 
     return (
       <div className="main_container">
-        <Header />
-
         <Outlet />
-
-        <Footer />
       </div>
     );
   }
--- src/component/pokedex/HeaderPokedex.js
+++ src/component/pokedex/HeaderPokedex.js
@@ -1,9 +1,38 @@
 import React from "react";
 
+import { NativeSelect } from "@mantine/core";
+
 export const HeaderPokedex = () => {
   return (
     <div className="header_pokedex">
-
+      <div className="filter_container">
+        <ul>
+          <li>
+            <NativeSelect 
+              label="Region"
+              data={[
+                {value: "", label: ""}
+              ]}
+            />
+          </li>
+          <li>
+            <NativeSelect 
+              label="Type"
+              data={[
+                {value: "", label: ""}
+              ]}
+            />
+          </li>
+          <li>
+            <NativeSelect 
+              label="Trier par..."
+              data={[
+                {value: "", label: ""}
+              ]}
+            />
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
