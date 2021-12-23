babb90632f9647974df225dc5d5cdd3b5ba2495a
christopher.cauet <christopher.cauet@sully-group.fr>
2021-12-23 16:21:10 +0100

    :bug: Fix bug

--- src/services/scripts/pokemon/getPokemonData.js
+++ src/services/scripts/pokemon/getPokemonData.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getPokemonData = async (name) => {
   const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-  const species = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.data.species.name}`);
+  const species = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.data.species.name}`);
 
   return { pokemon: pokemon.data, species: species.data }
 }
