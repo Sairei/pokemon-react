7b01b662f811681e6aecbd0cb02c998791eed4c3
Christopher CAUET <christophercauet@hotmail.fr>
2021-12-23 23:45:09 +0100

    :art: Add color on gender icon in pokemon page

--- src/utils/GenderRate.js
+++ src/utils/GenderRate.js
@@ -4,69 +4,76 @@ import { faMars, faVenus } from '@fortawesome/free-solid-svg-icons'
 
 export const genderRate = (genderRate) => {
 
-  console.log(genderRate);
+  const male =
+    <span style={{ color: "red" }}>
+      <FontAwesomeIcon icon={faMars} />
+    </span>
+  const female =
+    <span style={{ color: "blue" }}>
+      <FontAwesomeIcon icon={faVenus} />
+    </span>
 
   switch (genderRate) {
     case 0:
       return (
         <div>
-          <span>100% <FontAwesomeIcon icon={faVenus} /></span>
-          <span> 0% <FontAwesomeIcon icon={faMars} /></span>
+          <span>100% {female}</span>
+          <span> 0% {male}</span>
         </div>
       );
     case 1:
       return (
         <div>
-          <span>87.5% <FontAwesomeIcon icon={faVenus} /></span>
-          <span>  12.5% <FontAwesomeIcon icon={faMars} /></span>
+          <span>87.5% {female}</span>
+          <span>  12.5% {male}</span>
         </div>
       );
     case 2:
       return (
         <div>
-          <span>75% <FontAwesomeIcon icon={faVenus} /></span>
-          <span>  25% <FontAwesomeIcon icon={faMars} /></span>
+          <span>75% {female}</span>
+          <span>  25% {male}</span>
         </div>
       );
     case 3:
       return (
         <div>
-          <span>62.5% <FontAwesomeIcon icon={faVenus} /></span>
-          <span>  37.5% <FontAwesomeIcon icon={faMars} /></span>
+          <span>62.5% {female}</span>
+          <span>  37.5% {male}</span>
         </div>);
     case 4:
       return (
         <div>
-          <span>50% <FontAwesomeIcon icon={faVenus} /></span>
-          <span>  50% <FontAwesomeIcon icon={faMars} /></span>
+          <span>50% {female}</span>
+          <span>  50% {male}</span>
         </div>
       );
     case 5:
       return (
         <div>
-          <span>37.5% <FontAwesomeIcon icon={faVenus} /></span>
-          <span>  62.5% <FontAwesomeIcon icon={faMars} /></span>
+          <span>37.5% {female}</span>
+          <span>  62.5% {male}</span>
         </div>
       );
     case 6:
       return (
         <div>
-          <span>25% <FontAwesomeIcon icon={faVenus} /></span>
-          <span>  75% <FontAwesomeIcon icon={faMars} /></span>
+          <span>25% {female}</span>
+          <span>  75% {male}</span>
         </div>
       );
     case 7:
       return (
         <div>
-          <span>12.5% <FontAwesomeIcon icon={faVenus} /></span>
-          <span>  87.5% <FontAwesomeIcon icon={faMars} /></span>
+          <span>12.5% {female}</span>
+          <span>  87.5% {male}</span>
         </div>
       );
     case 8:
       return (
         <div>
-          <span>0% <FontAwesomeIcon icon={faVenus} /></span>
-          <span>  100% <FontAwesomeIcon icon={faMars} /></span>
+          <span>0% {female}</span>
+          <span>  100% {male}</span>
         </div>
       );
     default:
