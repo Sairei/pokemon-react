a88b8b081c1848209661e92cb1ece28c8f272046
christopher.cauet <christopher.cauet@sully-group.fr>
2021-12-23 15:59:35 +0100

    :sparkles: Get pokemon data on pokemon page

--- src/component/pokemon/PokemonPage.js
+++ src/component/pokemon/PokemonPage.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Container } from '@mantine/core';
 import { useParams } from 'react-router-dom';
 
+import { getPokemonData } from '../../services/scripts/pokemon/getPokemonData';
 import PokemonLeft from './PokemonLeft';
 import PokemonRight from './PokemonRight';
 
 const PokemonPage = () => {
+  const [pokemon, setPokemon] = useState();
+  const [species, setSpecies] = useState();
+
   const { id } = useParams();
 
-  console.log(id);
+  useEffect(() => {
+    async function fetchData() {
+      await getPokemonData(id)
+        .then(({ pokemon, species }) => {
+          setPokemon(pokemon);
+          setSpecies(species);
+        });
+    }
+
+    fetchData()
+  }, [])
 
   return (
     <Container className='info_container' size="xs" >
