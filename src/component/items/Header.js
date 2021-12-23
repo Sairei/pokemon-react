235ee3f9166f341c970b1d0594908b968e002915
christopher.cauet <christopher.cauet@sully-group.fr>
2021-12-23 09:32:17 +0100

    :sparkles: Add language dropdown

--- src/component/items/Header.js
+++ src/component/items/Header.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
+import { NativeSelect } from "@mantine/core";
+import Flags from 'country-flag-icons/react/3x2'
 
 const Header = () => {
+  const [flag, setFlag] = useState(
+    <Flags.US title="United States"/>
+  )
+
+  const handleChangeLang = (value) => {
+    // TODO : Creer un tableau qui liste tous les drapeaux possibles et une fonction pour récuperer le bon
+    setFlag(
+      <Flags.FR title={value.label}/>
+    )
+  }
+
   return (
     <div className="nav-bar-container">
       <ul className="middle-items">
@@ -20,6 +33,13 @@ const Header = () => {
           </Link>
         </li>
       </ul>
+      <NativeSelect className="language" 
+        data={[
+          {value: "US", label: "English"}, 
+          {value: "FR", label: "Français"}
+        ]}
+        onChange={(event) => handleChangeLang(event.currentTarget)}
+        icon={ flag }/>
     </div>
   )
 }
