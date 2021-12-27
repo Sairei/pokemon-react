e67740b348c3468d7c13cc3d30fe5f526ce672bf
christopher.cauet <christopher.cauet@sully-group.fr>
2021-12-27 17:49:00 +0100

    :hammer: Set how pokemon can evolve

--- src/utils/ConvertEvolsDetailToString.js
+++ src/utils/ConvertEvolsDetailToString.js
@@ -1,32 +1,50 @@
 export const convertEvolsDetailToString = (detail) => {
   let res = "Inconnu...";
 
-  
-  if (detail[0].held_item) {
-    if (detail[0].trigger.name === "trade")
-      res = "trade with item";
-    else if (detail[0].trigger.name === "level-up")
-      res = "level up with item";
-
-    res = <>{res}<br/>{"\"" + detail[0].held_item.name + "\""}</>;
-  } else if (detail[0].item) {
-    res = detail[0].item.name;
-  } else if (detail[0].min_happiness) {
-    if (detail[0].trigger.name === "trade")
-      res = "trade with";
-    else if (detail[0].trigger.name === "level-up")
-      res = "level up with";
-
-    res = <>{res}<br/>{"happiness at " + detail[0].min_happiness}</>;
-  } else if (detail[0].min_level) {
+  // Objet à donner
+  if (detail[0].item) {
+    res = "give " + detail[0].item.name;
+  }
+  // Niveau à atteindre
+  else if (detail[0].min_level) {
     res = "lvl " + detail[0].min_level;
-  } else {
+  }
+  // Autre méthode
+  else {
     if (detail[0].trigger.name === "trade")
       res = "trade";
+    else if (detail[0].trigger.name === "level-up")
+      res = "level up";
+
+    let isCondition = false;
+    // Objet pour à porter avant de faire quelque chose (lvl_up ou echange)
+    if (detail[0].held_item) {
+      res = <>{res}{isCondition ? " and" : " with"}<br />{"\"" + detail[0].held_item.name + "\""}</>;
+      isCondition = true;
+    }
+    // Joie minimum à avoir avant de faire quelque chose (lvl_up ou echange)
+    if (detail[0].min_happiness) {
+      res = <>{res}{isCondition ? " and" : " with"}<br />{"happiness at " + detail[0].min_happiness}</>;
+      isCondition = true;
+    }
+    // Affection minimale à avoir avant de faire quelque chose (lvl_up ou echange)
+    if (detail[0].min_affection) {
+      res = <>{res}{isCondition ? " and" : " with"}<br />{"affection at " + detail[0].min_affection}</>;
+      isCondition = true;
+    }
+    // Compétence d'un certain type à connaitre
+    if (detail[0].known_move_type) {
+      res = <>{res}{isCondition ? " and" : " with"}<br />{"when know type's move \"" + detail[0].known_move_type.name + "\""}</>;
+      isCondition = true;
+    }
+    // Localisation particuliere
+    if (detail[0].location) {
+      res = <>{res}{isCondition ? " and" : " with"}<br />{"in particular location"}</>;
+    }
   }
 
   if (detail[0].time_of_day)
-    res = <>{res}<br/>{"at " + detail[0].time_of_day}</>
+    res = <>{res}<br />{"at " + detail[0].time_of_day}</>
 
   return res;
 };
