43a22a7787a6503c658c3ae3a89c5ffdb552c4ae
christopher.cauet <christopher.cauet@sully-group.fr>
2021-12-27 17:09:33 +0100

    :hammer: Set how pokemon can evolve

--- src/utils/ConvertEvolsDetailToString.js
+++ src/utils/ConvertEvolsDetailToString.js
@@ -1,10 +1,32 @@
 export const convertEvolsDetailToString = (detail) => {
-  let res = "";
+  let res = "Inconnu...";
+
   
-  if(detail[0].min_level) 
+  if (detail[0].held_item) {
+    if (detail[0].trigger.name === "trade")
+      res = "trade with item";
+    else if (detail[0].trigger.name === "level-up")
+      res = "level up with item";
+
+    res = <>{res}<br/>{"\"" + detail[0].held_item.name + "\""}</>;
+  } else if (detail[0].item) {
+    res = detail[0].item.name;
+  } else if (detail[0].min_happiness) {
+    if (detail[0].trigger.name === "trade")
+      res = "trade with";
+    else if (detail[0].trigger.name === "level-up")
+      res = "level up with";
+
+    res = <>{res}<br/>{"happiness at " + detail[0].min_happiness}</>;
+  } else if (detail[0].min_level) {
     res = "lvl " + detail[0].min_level;
-  else
-    res = "Inconnu...";
+  } else {
+    if (detail[0].trigger.name === "trade")
+      res = "trade";
+  }
+
+  if (detail[0].time_of_day)
+    res = <>{res}<br/>{"at " + detail[0].time_of_day}</>
 
   return res;
 };
