92f1bb63e86710cd1b1bda6ac02e5c7ed4fd5e02
christopher.cauet <christopher.cauet@sully-group.fr>
2021-12-29 08:40:46 +0100

    :recycle: refacto

--- src/services/scripts/pokemon/getPokemonData.js
+++ src/services/scripts/pokemon/getPokemonData.js
@@ -1,11 +1,12 @@
 import axios from "axios";
+import { findImage } from "../../../utils/FindImage";
 import { getChain } from "../evolution/getChain";
 
 export const getPokemonData = async (name) => {
   const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
   const species = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.data.species.name}`);
 
-  let image = pokemon.data.sprites.other.dream_world.front_default ? pokemon.data.sprites.other.dream_world.front_default : pokemon.data.sprites.other['official-artwork'].front_default;
+  let image = findImage(pokemon.data);
   const evols = await getChain({ species: species.data, img: image })
 
   return { pokemon: pokemon.data, species: species.data, evols: evols }
