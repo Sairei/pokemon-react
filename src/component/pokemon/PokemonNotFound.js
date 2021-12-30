df2fdf8e316ef4fffb7f8aa722064abe6ce11e05
christopher.cauet <christopher.cauet@sully-group.fr>
2021-12-30 10:05:49 +0100

    :hammer: Create pokemon not found page

--- src/component/pokemon/PokemonNotFound.js
+++ src/component/pokemon/PokemonNotFound.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import { Image } from '@mantine/core';
+
+import pokeball from '../../assets/images/pokeball.png'
+
 const PokemonNotFound = ({ pokemonName }) => {
   return (
-    <div>
-      Pokemon {pokemonName} not found
+    <div className='pokemon_not_found'>
+      <div className='code_404'>
+        4
+        <Image
+          className='img'
+          height={250}
+          width={250}
+          src={pokeball} />
+        4
+      </div>
+      <div>
+        Pokemon "{pokemonName}" not found
+      </div>
     </div>
   );
 };
