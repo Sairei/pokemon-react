17f958b6a634262a0d4a1bd9f3bc2a55e3e7497a
Christopher CAUET <christophercauet@hotmail.fr>
2022-01-09 22:05:16 +0100

    :recycle: Convert class to function

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -1,29 +1,27 @@
-import { AppShell } from '@mantine/core';
 import React from 'react';
 
 import { Outlet } from 'react-router';
+import { AppShell } from '@mantine/core';
 
 import Header from './items/navigation/Header';
 import Navigation from './items/navigation/Navigation';
 
-class MainPage extends React.Component {
-  render() {
-    return (
-      <div className="main_container">
-        <AppShell
-          fixed
-          classNames={{
-            main: "main_body"
-          }}
-          navbar={<Navigation />}
-          header={<Header />}>
+const MainPage = () => {
+  return (
+    <div className="main_container">
+      <AppShell
+        fixed
+        classNames={{
+          main: "main_body"
+        }}
+        navbar={<Navigation />}
+        header={<Header />}>
 
-          <Outlet />
+        <Outlet />
 
-        </AppShell>
-      </div>
-    );
-  }
+      </AppShell>
+    </div>
+  );
 }
 
 export default MainPage;
