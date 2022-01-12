41ff16515d3ea05137e05b43716a6b1f6b8cb4f8
Sairei <christophercauet@hotmail.fr>
2022-01-12 09:24:04 +0100

    :bug: Fix zygarde image
    Pas d'image dans "home" pour le pokemon "zygarde"

--- src/services/utils/FindImage.js
+++ src/services/utils/FindImage.js
@@ -1,15 +1,13 @@
-export const findImage = (pokemon) => {
-  // const src = pokemon.sprites.other.dream_world.front_default
-  //   ? pokemon.sprites.other.dream_world.front_default
-  //   : pokemon.sprites.other['official-artwork'].front_default;
-  
-  const src = pokemon.sprites.other.home.front_default;
-  
+export const findImage = (pokemon) => {  
+  // const src = pokemon.sprites.other.home.front_default;
+  const src = pokemon.sprites.front_default
+
   return src;
 };
 
 export const findShiny = (pokemon) => {
-  const src = pokemon.sprites.other.home.front_shiny;
+  // const src = pokemon.sprites.other.home.front_shiny;
+  const src = pokemon.sprites.front_shiny
   
   return src;
 }
