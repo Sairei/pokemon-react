3f7bd368e3426c4ebd95998296d5616d628aeb8e
Sairei <christophercauet@hotmail.fr>
2022-01-19 13:52:44 +0100

    :hammer: Test horizontal scroll on phone

--- src/component/more/PokemonGeneral.js
+++ src/component/more/PokemonGeneral.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Image } from '@mantine/core';
+import { Image, ScrollArea } from '@mantine/core';
 
 import { capitalize } from '../../services/utils/Capitalize';
 
@@ -15,36 +15,38 @@ const PokemonGeneral = ({ pokemon, species }) => {
   // Partie générale d'un pokemon
   // https://bulbapedia.bulbagarden.net/wiki/Bulbasaur_(Pok%C3%A9mon)
   return (
-    <div className='more_info_general'>
-      <div className='presentation info_container_1'>
-        <div className='name_genera_id'>
-          <div className='name_genera info_container_2'>
-            <div className='pokemon_name'>
-              {capitalize(pokemon.name)}
+    <ScrollArea>
+      <div className='more_info_general'>
+        <div className='presentation info_container_1'>
+          <div className='name_genera_id'>
+            <div className='name_genera info_container_2'>
+              <div className='pokemon_name'>
+                {capitalize(pokemon.name)}
+              </div>
+              <div className='pokemon_genera'>
+                {genera}
+              </div>
             </div>
-            <div className='pokemon_genera'>
-              {genera}
+
+            <div className="pokemon_id info_container_2">
+              #{String(pokemon.id).padStart(3, '0')}
             </div>
           </div>
 
-          <div className="pokemon_id info_container_2">
-            #{String(pokemon.id).padStart(3, '0')}
+          <div className='pokemon_image info_container_2'>
+            <Image
+              src={pokemon.sprites.other['official-artwork'].front_default}
+              height={200}
+              fit="contain"
+            />
           </div>
         </div>
 
-        <div className='pokemon_image info_container_2'>
-          <Image
-            src={pokemon.sprites.other['official-artwork'].front_default}
-            height={200}
-            fit="contain"
-          />
+        <div>
+
         </div>
       </div>
-
-      <div>
-
-      </div>
-    </div>
+    </ScrollArea>
   );
 };
 
