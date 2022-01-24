358c8c96df80c761c8b62be957b80d178d91dda3
christopher.cauet <christopher.cauet@sully-group.fr>
2022-01-24 10:00:16 +0100

    :hammer: Add unvalaible sprite on generation

--- src/component/more/sprite/GenerationSprite.js
+++ src/component/more/sprite/GenerationSprite.js
@@ -3,12 +3,31 @@ import React from 'react';
 import GameSprite from './GameSprite';
 
 const GenerationSprite = ({ games }) => {
+  let unavailable = false;
+
+  const arr = Object.entries(games).map((values) => {
+    if (!values[1]['front_default']) {
+      unavailable = true;
+    }
+
+    return (
+      <GameSprite key={values[0]} games={values[0]} sprites={values[1]} />
+    );
+  });
+
+
+  if (unavailable) {
+    return (
+      <div>
+        This Pokémon was unavailable for this generation...
+      </div>
+    );
+  }
+
   return (
-    Object.entries(games).map((values) => {
-      return (
-        <GameSprite key={values[0]} games={values[0]} sprites={values[1]} />
-      );  
-    })
+    <>
+      {arr}
+    </>
   );
 };
 
