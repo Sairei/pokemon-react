07d68fd2df9c8d938844b4cece6f25151765ca32
Sairei <christophercauet@hotmail.fr>
2022-01-25 00:19:27 +0100

    :sparkles: Add "scroll to top" button

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { Outlet } from 'react-router';
-import { AppShell } from '@mantine/core';
+import { Affix, AppShell, Button, Transition } from '@mantine/core';
+import { useWindowScroll } from '@mantine/hooks';
 
 import Header from './UI/navigation/Header';
 import Navigation from './UI/navigation/Navigation';
 
 const MainPage = () => {
+  const [scroll, scrollTo] = useWindowScroll();
+
   return (
     <div className="main_container">
       <AppShell
@@ -20,6 +25,19 @@ const MainPage = () => {
         <Outlet />
 
       </AppShell>
+
+      <Affix position={{ bottom: 20, right: 20 }}>
+        <Transition transition="slide-up" mounted={scroll.y > 0}>
+          {(transitionStyles) => (
+            <Button
+            style={transitionStyles}
+            onClick={() => scrollTo({ y: 0 })}
+            >
+              <FontAwesomeIcon icon={faArrowUp} />
+            </Button>
+          )}
+        </Transition>
+      </Affix>
     </div>
   );
 }
